10b78606b48a4f182fe0882b4e95eabd3322d03d
AbhishekP63 <abhishekpatil36885@gmail.com>
2025-07-12 03:30:35 +0530

    updated for render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
@@ -119,4 +120,5 @@ def move():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
